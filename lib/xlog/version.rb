f60c3bfd18ed82e6da53dc47bb44f8de20af5b5a
Orest <orest@Orests-iMac-2.local>
2019-05-08 18:33:59 +0300

    Fix version

--- lib/xlog/version.rb
+++ lib/xlog/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Xlog
-  VERSION = '0.1.2'
+  VERSION = '0.1.2'.freeze
 end
--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,4 @@
 gem uninstall xlog
-rm -rf xlog-0.1.1.gem
+rm -rf xlog-0.1.2.gem
 gem build xlog.gemspec
-gem install xlog-0.1.1.gem
+gem install xlog-0.1.2.gem
