b2a4e0584af0429de66c34afea746cd4c700d38e
mikeKniaz <m.kniazevich@coaxsoft.com>
2022-03-09 11:35:31 +0200

    Remove response from raised error log

--- lib/xlog/middleware.rb
+++ lib/xlog/middleware.rb
@@ -9,7 +9,7 @@ module Xlog
     def call(env)
       @status, @headers, @response = @app.call(env)
     rescue StandardError => e
-      Xlog.and_raise_error(e, data: { request_params: Rack::Request.new(env).params, status: @status, headers: @headers, response: @response })
+      Xlog.and_raise_error(e, data: { request_params: Rack::Request.new(env).params, status: @status, headers: @headers })
     end
   end
 end
